752252706e1ba9939512fe4b7c1463592fdb7929
Artiom Sudin <artem.sudin@gmail.com>
2023-02-20 10:09:38 +0300

    Vehicle movement on navMesh trigger

--- Assets/Source/Scripts/Cars/CarMover.cs
+++ Assets/Source/Scripts/Cars/CarMover.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CarMover : MonoBehaviour
 {
+    [SerializeField] private Transform _targetPosition;
+    private NavMeshAgent _navmeshagent;
     private Rigidbody _rigidbody;
     private Vector3 _startPosition;
     private Vector3 _endPosition;
     private Vector3 _direction;
     private bool _isMoving = false;
+    private bool _hasEnteredTrigger = false;
     private float _speed = 20;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _navmeshagent = GetComponent<NavMeshAgent>();
+        _navmeshagent.updatePosition = false;
     }
 
     private void FixedUpdate()
@@ -19,6 +25,13 @@ public class CarMover : MonoBehaviour
         if (_isMoving)
         {
             _rigidbody.MovePosition(transform.position + _direction * _speed * Time.fixedDeltaTime);
+        }
+        
+        if (_hasEnteredTrigger)
+        {
+            _navmeshagent.enabled = true;
+            _navmeshagent.updatePosition = true;
+            _navmeshagent.destination = _targetPosition.position;
         }
     }
 
@@ -37,6 +50,14 @@ public class CarMover : MonoBehaviour
             _endPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
             _direction = (_endPosition - _startPosition).normalized;
             _isMoving = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Road road))
+        {
+            _hasEnteredTrigger = true;
         }
     }
 }
