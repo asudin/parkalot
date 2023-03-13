620082fe5386e8dab1f0285d56f8eddedc83b982
Artiom <artem.sudin@gmail.com>
2023-03-13 20:00:09 +0300

    Updated CarCollisionHandler script
    Script updated to include crash emojis and oncollision event

--- Assets/Source/Scripts/Cars/CarCollisionHandler.cs
+++ Assets/Source/Scripts/Cars/CarCollisionHandler.cs
@@ -1,22 +1,44 @@
 using System;
+using Random = UnityEngine.Random;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class CarCollisionHandler : MonoBehaviour
 {
+    [SerializeField] private Sprite[] _crashEmoji;
+
     private Car _car;
+    private Animator _animator;
 
     public event Action<Car> Collected;
 
     private void Awake()
     {
         _car = GetComponent<Car>();
+        _animator = GetComponent<Animator>();
     }
+
+    private void Crash(Sprite[] crashEmoji)
+    {
+        int randomEmoji = Random.Range(0, crashEmoji.Length);
+        Instantiate(crashEmoji[randomEmoji], transform.position, Quaternion.identity);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out WinningZoneTrigger zone))
         {
             Collected?.Invoke(_car);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Car car))
+        {
+            Crash(_crashEmoji);
+            _animator.Play("Crash");
         }
     }
 }
